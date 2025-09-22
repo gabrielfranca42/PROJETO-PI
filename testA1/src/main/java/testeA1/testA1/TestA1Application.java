190fbb97b01be2fc7d4b71833909d0e7ddffd2b8
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-09-22 14:09:46 -0300

    criando as camadas e adicionando o properties do mysql mas sem o link senha e usuario do banco de dados

---
+++ testA1/src/main/java/testeA1/testA1/TestA1Application.java
@@ -0,0 +1,13 @@
+package testeA1.testA1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestA1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestA1Application.class, args);
+	}
+
+}
