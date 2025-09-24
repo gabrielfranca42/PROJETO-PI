a8723a4ea4155e2c54c0e292cdceeadc78cc2c84
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-09-24 16:32:53 -0300

    criacao dos repositorys das duas tabelas ele sendo a de usuarios e a de sementes

--- testA1/src/main/java/Repository/SementeRepository.java
+++ testA1/src/main/java/Repository/SementeRepository.java
@@ -1,4 +1,9 @@
 package Repository;
 
-public interface SementeRepository {
+import ch.qos.logback.core.model.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SementeRepository extends JpaRepository <Model, Long> {
 }
--- testA1/src/main/java/Repository/UsuarioRepository.java
+++ testA1/src/main/java/Repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package Repository;
 
-public interface UsuarioRepository {
+import Model.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioModel , Long> {
+
+
+    Optional<UsuarioModel> findByUsername(String username);
 }
