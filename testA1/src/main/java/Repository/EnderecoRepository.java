d83543d1a01be5fcd8c400d0f137520fbf106603
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-10-08 16:29:01 -0300

    criao de repository de endereco estoquecontrole e estoque

--- testA1/src/main/java/Repository/EnderecoRepository.java
+++ testA1/src/main/java/Repository/EnderecoRepository.java
@@ -1,4 +1,9 @@
 package Repository;
 
-public interface EnderecoRepository {
+import Model.EnderecoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository <EnderecoModel ,Long> {
 }
--- testA1/src/main/java/Repository/EstoqueRepository.java
+++ testA1/src/main/java/Repository/EstoqueRepository.java
@@ -1,4 +1,9 @@
 package Repository;
 
-public interface EstoqueRepository {
+import Model.EstoqueModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstoqueRepository extends JpaRepository <EstoqueModel ,Long> {
 }
