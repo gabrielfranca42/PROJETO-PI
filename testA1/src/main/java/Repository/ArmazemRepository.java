34f3549baf2d7f84f6e4b87788629f6cdf8e5d83
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-10-08 16:20:54 -0300

    eu sou um animal e esqueci de adicionar os @Repository LoteRepository ArmazemRepository MunicipioRepository

--- testA1/src/main/java/Repository/ArmazemRepository.java
+++ testA1/src/main/java/Repository/ArmazemRepository.java
@@ -2,6 +2,8 @@ package Repository;
 
 import Model.ArmazemModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArmazemRepository extends JpaRepository <ArmazemModel, Long> {
 }
--- testA1/src/main/java/Repository/LoteRepository.java
+++ testA1/src/main/java/Repository/LoteRepository.java
@@ -2,6 +2,8 @@ package Repository;
 
 import Model.LoteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoteRepository extends JpaRepository<LoteModel, Long> {
 }
