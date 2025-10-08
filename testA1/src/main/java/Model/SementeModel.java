55f7b140449487e3d288e030724bdd3d4e91e06c
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-08 11:22:24 -0300

    consetando sementes baseado na modelagem e ja fazendo relacoes

--- testA1/src/main/java/Model/SementeModel.java
+++ testA1/src/main/java/Model/SementeModel.java
@@ -5,10 +5,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.List;
 //apenas criacao de entidade no banco de dados e nome da tabela
 
 
@@ -17,17 +16,42 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Builder
 @Table(name = "Semetes")
 
 public class SementeModel {
-//id e geracao de id automatico pelo mysql o tipo de estrategia e inclusive autoincremente
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    @Column(unique = true , nullable = false)
-    private String nomeSemente;
 
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+
+    @Column(name = "validade")
+    private Integer validade;
+
+
+    @Column(name = "especie", length = 45)
+    private String especie;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fornecimento_id", nullable = false)
+    private FornecimentoModel fornecimentoModel;
+
+
+    @OneToMany(mappedBy = "semente", fetch = FetchType.LAZY)
+    private List<EstoqueModel> estoqueModelList;
+
+
+    @OneToMany(mappedBy = "semente", fetch = FetchType.LAZY)
+    private List<LoteModel> loteModelList;
+
+
+    @OneToMany(mappedBy = "semente", fetch = FetchType.LAZY)
+    private List<LoteControleModel> loteControleModelList;
 
 
 
