75f618785a2dc903f87db856066b91e3cdc87dd6
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-10-08 16:32:12 -0300

    criacao de repository de fornecedor e loteControle

--- testA1/src/main/java/Repository/LoteControleRepository.java
+++ testA1/src/main/java/Repository/LoteControleRepository.java
@@ -1,4 +1,9 @@
 package Repository;
 
-public interface LoteControleRepository {
+import Model.LoteControleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoteControleRepository extends JpaRepository <LoteControleModel, Long> {
 }
