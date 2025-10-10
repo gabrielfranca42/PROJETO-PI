246ed2d79e8324cdb99734a5fd5d8f89d70ab410
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-10 08:43:52 -0300

    criando estoquecontrole dto

--- testA1/src/main/java/DTO/EstoqueControleDto.java
+++ testA1/src/main/java/DTO/EstoqueControleDto.java
@@ -1,4 +1,22 @@
 package DTO;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
 public class EstoqueControleDto {
+
+    private Long id;
+    private String localArmazenamento;
+    private BigDecimal quantidadeAtual;
+    private LocalDateTime dataUltimaAtualizacao;
+    private Long estoqueId;
+    private Long loteId;
+    private Long cooperativaId;
 }
