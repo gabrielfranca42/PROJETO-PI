3783d5d7b8d0a57d12e658d85362dfe1d453671f
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-10 09:12:49 -0300

    criando fornecimentodto e deleteando cadastrorResponsedto

---
+++ testA1/src/main/java/DTO/FornecimentoDto.java
@@ -0,0 +1,17 @@
+package DTO;
+
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class FornecimentoDto {
+
+    private Long id;
+    private Long fornecedorId;
+    private List<Long> sementeIds;
+}
