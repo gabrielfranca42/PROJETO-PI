87ff3666a930bd6fd5e9ec9d2128bf4ca20a955a
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-10 10:02:07 -0300

    criando LoteControleDto

--- testA1/src/main/java/DTO/LoteControleDto.java
+++ testA1/src/main/java/DTO/LoteControleDto.java
@@ -1,4 +1,24 @@
 package DTO;
 
+import ch.qos.logback.core.status.Status;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 public class LoteControleDto {
+
+
+    private Long id;
+    private Long loteId;
+    private Long sementeId;
+    private Long fornecedorId;
+    private LocalDateTime dataUltimaAtualizacao;
+    private LocalDate dataValidade;
+    private Status status;
 }
